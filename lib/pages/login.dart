6b704df858269b4a9f66bfac43646d2f0599f4c0
PIETRO6806 <pietro060806@gmail.com>
2024-02-23 00:21:05 -0300

    login and register pages modified

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:rifa_liceu_flutter/pages/home.dart';
-import 'package:rifa_liceu_flutter/pages/register.dart'; // Import the new page
+import 'package:rifa_liceu_flutter/pages/register.dart';
 
 class LoginPage extends StatelessWidget {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,31 +14,77 @@ class LoginPage extends StatelessWidget {
         backgroundColor: Colors.blueAccent,
         centerTitle: true,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                // Simulate successful login, you can replace this with actual authentication logic
-                // Once logged in, navigate to the home page
-                Navigator.pushReplacementNamed(context, '/home');
-              },
-              child: const Text('Login'),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              children: [
+                Image.asset(
+                  'assets/images/pascoaliceu.png', // Replace with the actual image path
+                  width: 50,
+                  height: 50,
+                ),
+                SizedBox(width: 16),
+                Text(
+                  'Login',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+              ],
             ),
-            SizedBox(height: 16),
-            GestureDetector(
-              onTap: () {
-                // Navigate to the register page
-                Navigator.pushNamed(context, '/register');
-              },
-              child: Text(
-                "Don't have an account yet? Register here",
-                style: TextStyle(color: Colors.blue),
-              ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextFormField(
+                  controller: emailController,
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                  ),
+                ),
+                SizedBox(height: 16),
+                TextFormField(
+                  controller: passwordController,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                  ),
+                ),
+                SizedBox(height: 16),
+                ElevatedButton(
+                  onPressed: () {
+                    // Simulate successful login, you can replace this with actual authentication logic
+                    // Access the entered email and password using the controllers
+                    String email = emailController.text;
+                    String password = passwordController.text;
+
+                    // You can perform authentication logic here
+                    // For simplicity, let's just print the email and password for now
+                    print('Email: $email, Password: $password');
+
+                    // Once logged in, navigate to the home page
+                    Navigator.pushReplacementNamed(context, '/home');
+                  },
+                  child: const Text('Login'),
+                ),
+                SizedBox(height: 16),
+                GestureDetector(
+                  onTap: () {
+                    // Navigate to the register page
+                    Navigator.pushNamed(context, '/register');
+                  },
+                  child: Text(
+                    "Don't have an account yet? Register here",
+                    style: TextStyle(color: Colors.blue),
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
--- lib/pages/register.dart
+++ lib/pages/register.dart
@@ -1,7 +1,13 @@
+// register.dart
+
 import 'package:flutter/material.dart';
 import 'package:rifa_liceu_flutter/pages/home.dart';
 
 class RegisterPage extends StatelessWidget {
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,31 +16,85 @@ class RegisterPage extends StatelessWidget {
         backgroundColor: Colors.blueAccent,
         centerTitle: true,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                // Simulate successful registration, you can replace this with actual registration logic
-                // Once registered, navigate to the home page
-                Navigator.pushReplacementNamed(context, '/home');
-              },
-              child: const Text('Register'),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              children: [
+                Image.asset(
+                  'assets/images/pascoaliceu.png', // Replace with the actual image path
+                  width: 50,
+                  height: 50,
+                ),
+                SizedBox(width: 16),
+                Text(
+                  'Register',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+              ],
             ),
-            SizedBox(height: 16),
-            GestureDetector(
-              onTap: () {
-                // Navigate back to the login page
-                Navigator.pop(context);
-              },
-              child: Text(
-                'Already have an account? Login here',
-                style: TextStyle(color: Colors.blue),
-              ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextFormField(
+                  controller: usernameController,
+                  decoration: InputDecoration(
+                    labelText: 'Username',
+                  ),
+                ),
+                SizedBox(height: 16),
+                TextFormField(
+                  controller: emailController,
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                  ),
+                ),
+                SizedBox(height: 16),
+                TextFormField(
+                  controller: passwordController,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                  ),
+                ),
+                SizedBox(height: 16),
+                ElevatedButton(
+                  onPressed: () {
+                    // Simulate successful registration, you can replace this with actual registration logic
+                    // Access the entered username, email, and password using the controllers
+                    String username = usernameController.text;
+                    String email = emailController.text;
+                    String password = passwordController.text;
+
+                    // You can perform registration logic here
+                    // For simplicity, let's just print the values for now
+                    print('Username: $username, Email: $email, Password: $password');
+
+                    // Once registered, navigate to the home page
+                    Navigator.pushReplacementNamed(context, '/home');
+                  },
+                  child: const Text('Register'),
+                ),
+                SizedBox(height: 16),
+                GestureDetector(
+                  onTap: () {
+                    // Close the register page and navigate back to the login page
+                    Navigator.popUntil(context, ModalRoute.withName('/login'));
+                  },
+                  child: Text(
+                    'Already have an account? Login here',
+                    style: TextStyle(color: Colors.blue),
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
